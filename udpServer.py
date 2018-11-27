90492fedce7dda88c740335db72734e98a38de6e
Daniil Naumetc <nullpvndv@gmail.com>
2018-11-27 18:02:24 +0100

    Create udpServer.py

---
+++ udpServer.py
@@ -0,0 +1,36 @@
+import socket
+import pickle, numpy
+
+class MemoryServer:
+	def __init__(self):
+		self.UDP_IP = "127.0.0.1"
+		self.UDP_PORT = 1337
+		# self.UDP_PORT_IN  = 228
+
+
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.sock.bind((self.UDP_IP, self.UDP_PORT))
+
+		# self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+	def serve(self):
+		while True:
+			data = self.sock.recvfrom(1024)
+			address = data[1]
+			data = data[0].decode('utf8')
+
+			if not data:
+				break
+
+			answer = f'KEK: {data}'
+
+			print('Received from:', address)
+			self.sock.sendto(answer.encode('utf8'), address)
+
+		# sock.sendto(message, (UDP_IP, UDP_PORT))
+
+a = MemoryServer()
+
+a.serve()
+# ncat -v localhost 1337 -u
