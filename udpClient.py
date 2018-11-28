d3436496e34b0bbd67566abeb26d30f22b25cb2b
Daniil <daniil.naumetc@gmail.com>
2018-11-28 23:16:08 +0100

    normal client

--- udpClient.py
+++ udpClient.py
@@ -5,21 +5,24 @@ import numpy as np
 class MemoryClient:
 	def __init__(self):
 		self.UDP_IP = "127.0.0.1"
-		self.UDP_PORT = 1337
+		self.UDP_PORT_SERVER = 1337
+		self.UDP_PORT = 1338
 		# self.UDP_PORT_IN  = 228
 
 
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		# self.sock.bind((self.UDP_IP, self.UDP_PORT))
+		self.sock.bind((self.UDP_IP, self.UDP_PORT))
 
 		# self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 
 	def send(self, data):
-		self.sock.sendto(pickle.dumps(data), (self.UDP_IP, self.UDP_PORT))
+		self.sock.sendto(pickle.dumps(data), (self.UDP_IP, self.UDP_PORT_SERVER))
+		return self.sock.recvfrom(1024)
 		# sock.sendto(message, (UDP_IP, UDP_PORT))
 
 a = MemoryClient()
 
-a.send(np.array([1,3,3,7]))
+print(a.send(np.array([1,3,3,7])))
+# print()
 # ncat -v localhost 1337 -u
